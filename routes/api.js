16e19f57195ac2c2702205b1fbe8681b29e792ee
Lareciyo <lmcneal@perseverenow.org>
2026-01-07 13:13:32 -0600

    seventh

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,42 @@
-'use strict';
+"use strict";
 
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-
   const convertHandler = new ConvertHandler();
 
-  app.route('/api/convert')
-    .get(function (req, res) {
-      const input = req.query.input;
+  app.route("/api/convert").get(function (req, res) {
+    const input = req.query.input;
 
-      const num = convertHandler.getNum(input);
-      const unit = convertHandler.getUnit(input);
+    const num = convertHandler.getNum(input);
+    const unit = convertHandler.getUnit(input);
 
-      if (num === "invalid number" && unit === "invalid unit") {
-        return res.send("invalid number and unit");
-      }
-      if (num === "invalid number") return res.send("invalid number");
-      if (unit === "invalid unit") return res.send("invalid unit");
+    if (num === "invalid number" && unit === "invalid unit") {
+      return res.send("invalid number and unit");
+    }
 
-      const returnUnitRaw = convertHandler.getReturnUnit(unit);
-      const returnNum = convertHandler.convert(num, unit);
+    if (num === "invalid number") {
+      return res.send("invalid number");
+    }
 
-      const initUnit = unit === "l" ? "L" : unit;
-      const returnUnit = returnUnitRaw === "l" ? "L" : returnUnitRaw;
+    if (unit === "invalid unit") {
+      return res.send("invalid unit");
+    }
 
-      res.json({
-        initNum: num,
-        initUnit: initUnit,
-        returnNum: Number(returnNum.toFixed(5)),
-        returnUnit: returnUnit,
-        string: convertHandler.getString(num, unit, Number(returnNum.toFixed(5)), returnUnitRaw)
-      });
+    const returnUnit = convertHandler.getReturnUnit(unit);
+    const returnNum = convertHandler.convert(num, unit);
+
+    res.json({
+      initNum: num,
+      initUnit: unit === "l" ? "L" : unit,
+      returnNum: Number(returnNum.toFixed(5)),
+      returnUnit: returnUnit === "l" ? "L" : returnUnit,
+      string: convertHandler.getString(
+        num,
+        unit,
+        Number(returnNum.toFixed(5)),
+        returnUnit
+      ),
     });
+  });
 };
